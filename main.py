d6026ad5802a67edb76acf922ddbd2198c877c08
T0p2 <nachoayerbe2003@gmail.com>
2023-10-19 15:45:34 -0300

    update

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import urllib.parse
 
 
 
-api_key = "api key"
+api_key = "AIzaSyCBfxe1t34R0ai0Xh_a9iCUKykDQ2x0ctc"
 
-
+list = []
 listener = sr.Recognizer() 
 name = "Alexa"
 voz = pyttsx3.init()
@@ -55,69 +55,86 @@ def alexa():
             print('Sorry could not hear')
             
     return(voice)
+
+
+
+
+
+def run_yt_video(voice):
+   
+    voice = voice.replace("reproduce", "reproduciendo")
+    talk(voice)
+
+    # Extraer el nombre de la canción
+    song = voice
+    for forma in list:
+        song = song.replace(forma, "")
+    pywhatkit.playonyt(song.strip())
+
+    
+def run_yt_sub(voice):
         
-    
+    canal_yt = voice
 
-
-    
-'''Aca lo que hacemos es verificar que el usuario haya dicho 
-Jarvis, reproduce. Si alguna de las dos falla no se escucha nada
-'''
-
-
-def run_yt():
-    voice = alexa()
-
-    # Reproducir algo en YouTube
-    formas_reproducir = ["reproduci", "reproduce"]
-
-    # Verificar si alguna forma está en la entrada de voz
-    if any(forma in voice for forma in formas_reproducir):
-        voice = voice.replace("reproduce", "reproduciendo")
-        talk(voice)
-
-        # Extraer el nombre de la canción
-        song = voice
-        for forma in formas_reproducir:
-            song = song.replace(forma, "")
-        pywhatkit.playonyt(song.strip())
-
-    else:
-
-       # Saber los subscriptores de un canal de YouTube
-        formas_subs = ["cuantos Subs tiene", "cuantos subscriptores tiene", "subs de", "decime los subscriptores de"]
-
-        if any(forma in voice for forma in formas_subs):
-            canal_yt = voice
-
-            #reemplaza la forma que tenemos para identificar que se quiere por blanco
-            for forma in formas_subs:
-                canal_yt = canal_yt.replace(forma, "")
+    #reemplaza la forma que tenemos para identificar que se quiere por blanco
+    for forma in list:
+        canal_yt = canal_yt.replace(forma, "")
 
 
     # Hacemos una limpieza en el nombre del canal para que este sin espacios
-            canal_yt = urllib.parse.quote(canal_yt)
-            canal_yt = canal_yt.replace("%20", "")
+    canal_yt = urllib.parse.quote(canal_yt)
+    canal_yt = canal_yt.replace("%20", "")
 
 
-        # Encontramos el canal de YouTube, nos devuelve un JSON, con ese JSON buscamos en statistics la cantidad de sub que tiene
-            data = json.loads(urllib.request.urlopen(f'https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername={canal_yt.strip()}&key={api_key}').read().decode('utf-8'))
+    # Encontramos el canal de YouTube, nos devuelve un JSON, con ese JSON buscamos en statistics la cantidad de sub que tiene
+    data = json.loads(urllib.request.urlopen(f'https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername={canal_yt.strip()}&key={api_key}').read().decode('utf-8'))
     
     #evaluamos si se encunetran resultados para el canal
-        if data["pageInfo"]["totalResults"] == 0:
-             talk("No se encontraron resultados para el canal.")
-        else: 
+    if data["pageInfo"]["totalResults"] == 0:
+        talk("No se encontraron resultados para el canal.")
+    else: 
 
-        #agarramos del json el direccionario de items y extraemos los subs
-            items = data.get('items', [])
-            first_item = items[0]
-            subs = first_item['statistics']['subscriberCount']
+    #agarramos del json el direccionario de items y extraemos los subs
+        items = data.get('items', [])
+        first_item = items[0]
+        subs = first_item['statistics']['subscriberCount']
 
 
 
-            # Imprimir la cantidad de suscriptores
-            print(f"{canal_yt} tiene {subs} suscriptores.")
-            talk(f"{canal_yt} tiene {subs} suscriptores.")
+        # Imprimir la cantidad de suscriptores
+        print(f"{canal_yt} tiene {subs} suscriptores.")
+        talk(f"{canal_yt} tiene {subs} suscriptores.")
 
-run_yt()
 
+
+
+        
+
+def call_functions():
+    input = alexa()
+
+# Reproducir algo en YouTube
+
+    list = ["reproduci", "reproduce"]
+
+ # Verificar si alguna forma está en la entrada de voz
+    if any(i in input for i in list):
+            run_yt_video(input)
+    
+    else:
+
+    #Saber los subs de un canal
+        list = ["cuantos Subs tiene", "cuantos subscriptores tiene", "Subs in youtube de ", "in Youtube"]
+
+        if any (i in input for i in list):
+            run_yt_sub(input)
+        
+
+
+        
+            
+
+    
+
+
+call_functions()
